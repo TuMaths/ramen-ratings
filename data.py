2f56b2c030e0a7bdef523754beec9c557726d644
TuMaths <39281297+TuMaths@users.noreply.github.com>
2018-05-16 00:22:42 -0700

    Rev1
    Cleaned up Data Import, practicing creating categories. Need to think about getting numbers out as floats instead of strings.

--- data.py
+++ data.py
@@ -1,6 +1,22 @@
 #Ramen-Ratings
 
+#Initialize modules
 import numpy as np
-data = np.genfromtxt("ramen-ratings.csv", encoding = 'utf-8', skip_header = 1, delimiter = ",")
+
+#Import data
+data = np.genfromtxt("ramen-ratings.csv", dtype = 'U75', encoding = 'utf-8', skip_header = 1, delimiter = ",")
 ramen_ratings = np.array(data)
-print(ramen_ratings)
+
+#Define Common functs
+#Compare a string against a column, return the vector of BOOL values
+def comparison_vector(input_array,string,col):
+    is_it = input_array[:,col] == string
+    return is_it
+
+#Practice creating data catagories
+rating_4 = comparison_vector(ramen_ratings,"4",5)
+rating_5 = comparison_vector(ramen_ratings,"5",5)
+country_japan = comparison_vector(ramen_ratings,"Japan",4)
+country_china = comparison_vector(ramen_ratings,"China",4)
+
+print(ramen_ratings[(rating_5)&(country_japan)])
