c905f4fee1f612eb0d8813d9bf83c2be81bb0ec4
TuMaths <39281297+TuMaths@users.noreply.github.com>
2018-05-23 17:01:11 -0700

    Data Vis update
    Looking at ratios, giving a score. This data needs more numbers.

--- data.py
+++ data.py
@@ -32,13 +32,35 @@ countries_5_stars[:10].plot(kind='bar')     #Plot the first 10 countries number 
 country_list = five_stars['country'].unique()
 
 #Find the ratio between 5 star ramens over all ramens reviewed from that country
-five_star_ratio = {}
+five_star_ratio_country = {} #Ratio of 5star vs country submitted ramen
+five_star_ratio_overall = {} #Ratio of 5star vs overall submitted ramen total
+overall_ratio = {}           #Ratio of Country submitted ramen vs overall total
+overall_points = {}
+
+total_reviews = countries.sum()
+total_5_reviews = countries_5_stars.sum()
+
 for each in country_list:
     total_count = countries[each]
     five_count = countries_5_stars[each]
-    five_star_ratio[each] = five_count / total_count
+    ratio_country = five_count / total_count
+    ratio_overall = total_count / total_reviews
+    ratio_five_overall = five_count / total_reviews
+    five_star_ratio_country[each] = ratio_country
+    five_star_ratio_overall[each] = ratio_five_overall
+    overall_ratio[each] = ratio_overall
+    overall_points[each] = ratio_country + ratio_five_overall + ratio_overall
 
-#Plot that dictionary, get a visual representation of the five_star_ratio
-plt.bar(range(len(five_star_ratio)),list(five_star_ratio.values()), align='center')
-plt.xticks(range(len(five_star_ratio)),list(five_star_ratio.keys()))
+#Plot those dictionaries, get a visual representation of the five_star_ratio
+plt.bar(range(len(five_star_ratio_country)),list(five_star_ratio_country.values()), align='center')
+plt.xticks(range(len(five_star_ratio_country)),list(five_star_ratio_country.keys()))
+plt.show()
+plt.bar(range(len(five_star_ratio_overall)),list(five_star_ratio_overall.values()), align='center')
+plt.xticks(range(len(five_star_ratio_overall)),list(five_star_ratio_overall.keys()))
+plt.show()
+plt.bar(range(len(overall_ratio)),list(overall_ratio.values()), align='center')
+plt.xticks(range(len(overall_ratio)),list(overall_ratio.keys()))
+plt.show()
+plt.bar(range(len(overall_points)),list(overall_points.values()), align='center')
+plt.xticks(range(len(overall_points)),list(overall_points.keys()))
 plt.show()
